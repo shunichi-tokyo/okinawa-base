5dc2d80042c717e48b9b8e533d86a1b9faa7f7d7
EC2 Default User <ec2-user@ip-172-31-47-189.ap-northeast-1.compute.internal>
2023-12-11 14:40:59 +0000

    [Add]ユーザー情報作成中

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,2 +1,29 @@
 class Public::UsersController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :is_active, :profile_image)
+  end
+
+
+
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :profile_image
+
+  def get_image
+    if image.attached?
+      image
+    else
+      'no_image.jpg'
+    end
+  end
+
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
@@ -11,5 +21,13 @@ class User < ApplicationRecord
       # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
       # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
     end
+  end
+
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+      profile_image.variant(resize_to_limit: [width, height]).processed
   end
 end
