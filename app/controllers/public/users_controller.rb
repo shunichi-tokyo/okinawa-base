a5ed5dc33c12b91801aaadfb9bcc69d8cd3678f6
EC2 Default User <ec2-user@ip-172-31-47-189.ap-northeast-1.compute.internal>
2023-12-12 15:05:21 +0000

    [Add]ユーザー編集機能実装

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,25 +1,35 @@
 class Public::UsersController < ApplicationController
 
-  def new
-  end
-
-  def create
-  end
+  before_action :ensure_user, only: [:edit, :update, :destroy]
 
   def show
     @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :introduction, :is_active, :profile_image)
+  end
+  
+  def ensure_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to user_path(current_user)
+    end
   end
 
 
