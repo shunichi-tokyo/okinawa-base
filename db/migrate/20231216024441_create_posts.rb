1c5b25b52bc3aaaed51badcf0908f6b18c7cdac7
EC2 Default User <ec2-user@ip-172-31-47-189.ap-northeast-1.compute.internal>
2024-01-13 06:22:20 +0000

    [fix]mgファイルを修正

--- db/migrate/20231216024441_create_posts.rb
+++ db/migrate/20231216024441_create_posts.rb
@@ -4,13 +4,13 @@ class CreatePosts < ActiveRecord::Migration[6.1]
 
       t.timestamps
       t.integer :user_id, null: false
-      #t.string  :area, null: false
+      t.string  :area, null: false
       t.string  :event_name, null: false
       t.text    :event_detail, null: false
-      #t.string  :address, null: false
-      #t.date    :period, null: false
-      #t.string  :tag_index, null: false
-      #t.integer :price, null: false
+      t.string  :address, null: false
+      t.date    :period, null: false
+      t.string  :tag_index, null: false
+      t.integer :price, null: false
     end
   end
 end
