4c928c6b155deb048826ca3f9cec2c5e6a41992c
EC2 Default User <ec2-user@ip-172-31-47-189.ap-northeast-1.compute.internal>
2024-01-19 12:57:07 +0000

    [Fix]Admin権限ファイル修正

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -3,7 +3,7 @@
 class Admin::SessionsController < Devise::SessionsController
   #before_action :configure_sign_in_params, only: [:create]
   before_action :configure_permitted_parameters, only: [:create]
-
+  
 
   def after_sign_in_path_for(resource)
     #flash[:notice] = "Signed in successfully."
