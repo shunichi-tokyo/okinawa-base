ee152c036f377f4e9825e2c8a1506b3a942e7e0d
EC2 Default User <ec2-user@ip-172-31-47-189.ap-northeast-1.compute.internal>
2024-01-21 01:25:46 +0000

    [Fix]postsコントローラー修正

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,5 +1,5 @@
 class Public::PostsController < ApplicationController
-
+  before_action :authenticate_user!, except: [:top, :index]
   before_action :is_matching_login_user, only: [:edit, :update]
 
   def new
